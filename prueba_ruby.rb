d55c60907da26f9f32000dcde929fd83ab7296f2
Giorgio Zanni <g.zanni@hotmail.com>
2019-01-10 10:23:16 -0300

    ADD: file reading method to extract data

---
+++ prueba_ruby.rb
@@ -0,0 +1,8 @@
+def read_alum(file_name)
+file = File.open(file_name, 'r')
+alum = file.readlines.map(&:chomp).map { |lines| lines.split(', ') }
+file.close
+alum
+end
+
+print read_alum('notas.csv')
